b2b2c8ad6b79fb37b0825cfb9f0f181aefe00356
Mooit <98130495@student.uts.edu.au>
2017-02-15 00:14:00 -0500

    added sql query file

---
+++ queries.sql
@@ -0,0 +1,11 @@
+SELECT Name
+FROM washington_wizards
+GROUP BY name;
+
+INSERT
+INTO cleveland_cavaliers (number, name, age, height, weight) VALUES (
+'0', 'Chris Andersen', '38', '6'10', '245');
+
+SELECT *
+FROM new_york_knicks
+WHERE height like "7%";
